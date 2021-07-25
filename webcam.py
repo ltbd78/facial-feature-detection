c5574c24d60de7425feab42922b0e3c54414e907
ltbd78 <ltbd78@gmail.com>
2021-07-25 03:02:37 -0400

    updated website

--- webcam.py
+++ webcam.py
@@ -1,48 +1,76 @@
 import cv2
+import torchvision.transforms as T
 
-from core.models import*
+from core.models import *
 from core.networks import *
 from core.utils import *
 
 
 class WebCam:
-    def __init__(self, path_model, path_fc, tolerance):
-        
-        self.cascade = cv2.CascadeClassifier(path_fc)
+    def __init__(self, features, path_cascade, path_model, tolerance=10, resolution=(1280, 720)):
+        self.features = features
         self.tolerance = tolerance
+        self.resolution = resolution
+        
+        self.device = torch.device('cpu')
+        network_resnet = ResNet(34, len(features), in_channels=3).to(self.device)
+        self.model_cls = ModelSigmoidClassifier(network_resnet)
+        self.model_cls.load(path_model)
+        self.transforms = T.Compose([
+            T.Resize(size=(224, 224))])
+        self.cascade = cv2.CascadeClassifier(path_cascade)
+        self.cap = cv2.VideoCapture(0)
 
     def feed(self):
-        def extractFaceCoords(img, cascade, tolerance):
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            face_coords = cascade.detectMultiScale(gray, 1.2, tolerance, minSize=(60, 60))
-            return face_coords
-
-        cap = cv2.VideoCapture(0)
-
         while True:
-            ret, frame = cap.read()
-            face_coords = extractFaceCoords(frame, self.cascade, self.tolerance)
-            if face_coords is not None:
-                for coords in face_coords:
-                    x, y, w, h = coords
-                    face_rgb = cv2.cvtColor(frame[y:y + h, x:x + h], cv2.COLOR_BGR2RGB)
-                    img_fastai = Image(pil2tensor(face_rgb, np.float32).div_(255))
-                    prediction = self.learn.predict(img_fastai)
-                    if int(prediction[0]) == 1:
-                        result = self.Feature + ': True'
+            ret, frame = self.cap.read()
+            frame = cv2.resize(frame, self.resolution)
+            coords = get_coords(frame, self.cascade, self.tolerance, multi=True)
+
+            if coords is not None:
+                # BATCH PREPROCESSING
+                x_batch = None
+                for coord in coords:
+                    x, y, w, h = coord
+                    img = cv2.cvtColor(frame[y:y + h, x:x + h], cv2.COLOR_BGR2RGB)
+                    img = img.transpose(2, 0, 1)
+                    x_ = torch.tensor(img, device=self.device, dtype=torch.float32).unsqueeze(0)
+                    x_ = self.transforms(x_)
+                    if x_batch is None:
+                        x_batch = x_
                     else:
-                        result = self.Feature + ': False'
-                    p = prediction[2].tolist()
-                    prob = 'probability: ' + str(round(p[1], 3))
-                    cv2.rectangle(img=frame, pt1=(x, y), pt2=(x + w, y + h), color=(255, 255, 0),
-                                  thickness=1)  # color in BGR
-                    cv2.putText(img=frame, text=prob, org=(x, y - 13), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.5,
-                                color=(0, 255, 0), thickness=1)
-                    cv2.putText(img=frame, text=result, org=(x, y - 26), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                fontScale=.5,
-                                color=(0, 255, 0), thickness=1)
+                        x_batch = torch.cat([x_batch, x_])
+
+                # BATCH PREDICTIONS
+                y_pred = self.model_cls.network(x_batch)
+                probs = torch.sigmoid(y_pred).cpu().detach().numpy()
+
+                # OUTPUT
+                for i in range(probs.shape[0]):
+                    x, y, w, h = coords[i]
+                    labels = {self.features[j]:probs[i][j] for j in range(len(probs[i]))}
+
+                    cv2.rectangle(
+                        img=frame,
+                        pt1=(x, y),
+                        pt2=(x + w, y + h),
+                        color=(255, 255, 0), # BGR
+                        thickness=1)
+
+                    d = 13
+                    for k, v in labels.items():
+                        cv2.putText(
+                            img=frame,
+                            text=f'{k}:{v:3.2f}',
+                            org=(x, y + h + d),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=.5,
+                            color=(0, 255, 0), # BGR
+                            thickness=1)
+                        d += 13
 
             ret, jpeg = cv2.imencode('.jpg', frame)
             output = jpeg.tobytes()
+            
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + output + b'\r\n\r\n')
--- webpage.py
+++ webpage.py
@@ -1,44 +1,39 @@
 from flask import Flask, render_template, Response, url_for
 from webcam import WebCam
 
-# TODO: multi-threading, socket, frame rate
-# TODO: start, stop, record, save
-# TODO: more features (register, login)
-# TODO: tolerance adjustment
-# TODO: log visitors
 
 app = Flask(__name__)
-path = "C:\\Users\\Linsu Han\\Documents\\[GITHUB]\\facial-feature-detection"
-cascade_path = path + '\\resources\\haarcascade_frontalface_default.xml'
-model = 'resnet50'
-tolerance = 3
-posts = [{'name': 'Linsu Han', 'uni': 'lh2910'},
-         {'name': 'Zora Li', 'uni': 'xl2788'}]
 
+path_cascade = './resources/haarcascade_frontalface_default.xml'
+path_model = './resources/models/cls.pth'
+tolerance = 10
+features = ['Attractive', 'Bags_Under_Eyes', 'Bangs', 'Chubby', 'Eyeglasses', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Smiling', 'Wearing_Lipstick', 'Young']
 
 @app.route('/')
 def index():
-    return render_template('index.html', posts=posts)
-
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/detect/<int:resolution>')
+def detect(resolution):
+    title = f'Multi-Label Classification - {resolution}p'
+    return render_template('detect.html', resolution=resolution, title=title)
 
-@app.route('/detect/<feature>')
-def detect(feature):
-    feature_name = ' '.join(feature.split('_')).title()
-    return render_template('detect.html', feature=feature, title=feature_name)
-
-
-@app.route('/webcam/<feature>')
-def webcam(feature):
-    feature_name = ' '.join(feature.split('_')).title()
-    learner_path = path + f'\\resources\\models\\{feature_name}_{model}.pkl'
-    cam = WebCam(feature_name, learner_path, cascade_path, tolerance)
+@app.route('/webcam/<int:resolution>')
+def webcam(resolution):
+    if resolution == 1080:
+        resolution_ = (1920, 1080)
+    elif resolution == 720:
+        resolution_ = (1280, 720)
+    elif resolution == 480:
+        resolution_ = (640, 480)
+    elif resolution == 360:
+        resolution_ = (480, 360)
+    cam = WebCam(features, path_cascade, path_model, tolerance=tolerance, resolution=resolution_)
     return Response(cam.feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)  # set false in production
+    app.run(debug=True) # set debug=false in production
