c8451b87ed47e7c347465f9ea946d474a52cfd33
ltbd78 <ltbd78@gmail.com>
2021-07-25 00:57:43 -0400

    bugfix: now returns None instead of empty tuple

--- core/utils.py
+++ core/utils.py
@@ -4,13 +4,14 @@ def get_coords(img_cv2, fc, tolerance, multi=False):
     gray = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2GRAY)
     coords = fc.detectMultiScale(gray, 1.2, tolerance, minSize=(60, 60))
     
+    if len(coords) == 0:
+        return None
+    
     if multi:
         return coords
     else:
         if len(coords) == 1:
             return coords[0]
-        elif len(coords) == 0:
-            return None
         else:
             max_area = 0
             index = 0
